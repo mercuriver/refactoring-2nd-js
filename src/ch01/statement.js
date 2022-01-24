58fe7521d30c0c371121cd27d928496a8bdbf529
mercuriver <mercuriver@gmail.com>
2022-01-24 23:04:27 +0900

    feat(ch01): Run sample code

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -1,4 +1,4 @@
-function statement(invoice, plays) {
+const statement = (invoice, plays) => {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -44,4 +44,6 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits} 점\n`;
   return result;
-}
+};
+
+export default statement;
