9bd606ed7fc97a5fac3c30262bc303f7adc6e317
mercuriver <mercuriver@gmail.com>
2022-01-24 23:58:49 +0900

    refactor(ch01): 함수 선언 바꾸기 (ch6.5), 화폐 단위를 다루기에 usd로 변경

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -30,7 +30,7 @@ const statement = (invoice, plays) => {
     }
     return result;
   };
-  const format = (aNumber) =>
+  const usd = (aNumber) =>
     new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
@@ -42,12 +42,12 @@ const statement = (invoice, plays) => {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits} 점\n`;
   return result;
 };
