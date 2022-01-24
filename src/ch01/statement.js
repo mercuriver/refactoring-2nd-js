61a2c0fb0b807ca66c42c2ae723b3091764a1dfb
mercuriver <mercuriver@gmail.com>
2022-01-24 23:53:29 +0900

    refactor(ch01): 불필요 주석 정리

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -9,13 +9,13 @@ const statement = (invoice, plays) => {
     let result = 0;
 
     switch (playFor(aPerformance).type) {
-      case "tragedy": // 비극
+      case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
           result += 1000 * (aPerformance.audience - 30);
         }
         break;
-      case "comedy": // 희극
+      case "comedy":
         result = 30000;
         if (aPerformance.audience > 20) {
           result += 10000 + 500 * (aPerformance.audience - 20);
@@ -29,13 +29,10 @@ const statement = (invoice, plays) => {
   };
   const volumeCreditsFor = (aPerformance) => {
     let result = 0;
-    // 포인트를 적립한다.
     result += Math.max(aPerformance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === playFor(aPerformance).type) {
       result += Math.floor(aPerformance.audience / 5);
     }
-
     return result;
   };
 
@@ -44,7 +41,6 @@ const statement = (invoice, plays) => {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
