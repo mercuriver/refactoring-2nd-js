894c19c4e9ec2be7088b18fc15be6b1e82861ac9
mercuriver <mercuriver@gmail.com>
2022-01-25 20:46:53 +0900

    refactor(ch01): 생성자를 팩터리 함수로 바꾸기(ch11.8)

--- src/ch01/createStatementData.js
+++ src/ch01/createStatementData.js
@@ -36,8 +36,19 @@ class PerformanceCalculator {
   }
 }
 
-const createPerformanceCalculator = (aPerformance, aPlay) =>
-  new PerformanceCalculator(aPerformance, aPlay);
+class TragedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {}
+
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+};
 
 const createStatementData = (invoice, plays) => {
   const playFor = (aPerformance) => plays[aPerformance.playID];
