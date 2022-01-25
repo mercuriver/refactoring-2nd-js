187c3a5600a33b1483b036de2c7fb0f3aeec743d
mercuriver <mercuriver@gmail.com>
2022-01-25 20:05:30 +0900

    fix(ch01): amount 접근 오류 수정

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -9,7 +9,7 @@ const renderPlainText = (data) => {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(data.amount)} (${perf.audience}석)\n`;
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(data.totalAmount)}\n`;
