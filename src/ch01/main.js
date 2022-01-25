762c2b5e9ba503cd9616d19da9fb4dd278c24f1e
mercuriver <mercuriver@gmail.com>
2022-01-25 20:23:43 +0900

    feat(ch01): html render 작성

--- src/ch01/main.js
+++ src/ch01/main.js
@@ -1,9 +1,10 @@
 import { readJSON } from "../util/fileController.js";
-import statement from "./statement.js";
+import statement, { htmlStatement } from "./statement.js";
 
 const invoices = readJSON("src/ch01/invoices.json");
 const plays = readJSON("src/ch01/plays.json");
 
 invoices.forEach((invoice) => {
   console.log(statement(invoice, plays));
+  console.log(htmlStatement(invoice, plays));
 });
