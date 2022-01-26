3dfbdd77f10e4b797277097b1a10e93aa7f2a4a2
mercuriver <mercuriver@gmail.com>
2022-01-26 17:35:13 +0900

    test(ch04): Add province/empty string demand

--- src/ch04/province.test.js
+++ src/ch04/province.test.js
@@ -40,6 +40,12 @@ describe("province", () => {
     expect(asia.shortfall).equal(-26);
     expect(asia.profit).equal(-10);
   });
+
+  it("empty string demand", function () {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
 });
 
 describe("no producers", () => {
