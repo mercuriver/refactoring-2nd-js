8db2c306251a9581672b12d71ad3016529031c1b
mercuriver <mercuriver@gmail.com>
2022-01-26 21:23:53 +0900

    refactor(ch06): 6.5 함수 선언 바꾸기, 매개변수 마이그레이션

--- src/ch06/05-1.js
+++ src/ch06/05-1.js
@@ -6,17 +6,13 @@ class Book {
     return this._reservations;
   }
 
-  addReservation(customer) {
-    this.zz_addReservation(customer, false);
-  }
-
-  zz_addReservation(customer, isPriority) {
+  addReservation(customer, isPriority) {
     assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
 }
 
 const bookcafe = new Book();
-bookcafe.addReservation({ name: "A" });
-bookcafe.addReservation({ name: "B" });
+bookcafe.addReservation({ name: "A" }, false);
+bookcafe.addReservation({ name: "B" }, true);
 console.log(bookcafe.reservation);
