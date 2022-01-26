ccc5e0b91a72c3869eb5b2547e310192bebe34b1
mercuriver <mercuriver@gmail.com>
2022-01-26 18:45:48 +0900

    refactor(ch06): for 반복문 파이프라인 적용

--- src/ch06/01.js
+++ src/ch06/01.js
@@ -3,27 +3,24 @@ const printBanner = () => {
   console.log("**** 고객 채무 ****");
   console.log("*****************");
 };
-function calculateOutstanding(invoice) {
-  let result = 0;
-  for (const o of invoice.orders) {
-    result += o.amount;
-  }
-  return result;
-}
 
-function recordDueDate(invoice) {
+const calculateOutstanding = (invoice) =>
+  invoice.orders.reduce((result, order) => (result += order.amount), 0);
+
+const recordDueDate = (invoice) => {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
-}
-function printDetails(invoice, outstanding) {
+};
+
+const printDetails = (invoice, outstanding) => {
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`);
-}
+};
 
 const printOwing = (invoice) => {
   printBanner();
