3eb278339691b5bac610f7fcfb84fd98824d1cab
mercuriver <mercuriver@gmail.com>
2022-01-26 17:06:31 +0900

    refactor(ch04): beforeEach를 통해 테스트 공용 변수 선언

--- src/ch04/province.test.js
+++ src/ch04/province.test.js
@@ -5,18 +5,20 @@ import Province from "./province.js";
 import { sampleProvinceData } from "./util.js";
 
 describe("province", () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+
   it("shortfall - assert", () => {
-    const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
   });
 
   it("shortfall - expect", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
