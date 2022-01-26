7b035ece88b11117dd587cb491aa46fd1366d813
mercuriver <mercuriver@gmail.com>
2022-01-26 18:35:51 +0900

    refactor(baseCode): ch06, add comment

--- baseCode/ch06/01.js
+++ baseCode/ch06/01.js
@@ -5,10 +5,12 @@ const printOwing = (invoice) => {
   console.log("**** 고객 채무 ****");
   console.log("*****************");
 
+  // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
+  // 마감일(dueDate)을 기록한다.
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
@@ -16,6 +18,7 @@ const printOwing = (invoice) => {
     today.getDate() + 30
   );
 
+  // 세부 사항을 출력한다.
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`);
