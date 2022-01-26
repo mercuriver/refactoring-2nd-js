2e87940e59c943128c43602dcca0f7de977e7238
mercuriver <mercuriver@gmail.com>
2022-01-26 17:04:39 +0900

    test(ch04): Add province/profit

--- src/ch04/province.test.js
+++ src/ch04/province.test.js
@@ -1,12 +1,22 @@
 import { describe, it } from "mocha";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 import Province from "./province.js";
 import { sampleProvinceData } from "./util.js";
 
 describe("province", () => {
-  it("shortfall", () => {
+  it("shortfall - assert", () => {
     const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
+  });
+
+  it("shortfall - expect", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.shortfall).equal(5);
+  });
+
+  it("profit", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).equal(230);
   });
 });
