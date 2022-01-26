7f7a82968bd425d2eba9ff3a9024e7286c98b963
mercuriver <mercuriver@gmail.com>
2022-01-26 23:20:58 +0900

    refactor(ch06): 6.11 단계 쪼개기

--- src/ch06/11-1.js
+++ src/ch06/11-1.js
@@ -7,19 +7,25 @@ const shippingMethod = {
   discountThreshold: 0.12,
 };
 
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountFee
+      : shippingMethod.feePerCase;
+
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
+
 const priceOrder = (product, quantity, shippingMethod) => {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+
+  return applyShipping(basePrice, shippingMethod, quantity, discount);
 };
 
 products.forEach((product) => {
