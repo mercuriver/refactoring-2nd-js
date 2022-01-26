1fe45d656e98d41b0293ccfd8830d38e77b0b5a0
mercuriver <mercuriver@gmail.com>
2022-01-26 22:09:34 +0900

    refactor(ch06): 6.6 변수 캡슐화하기, 값 캡슐화

--- src/ch06/06-1-defaultOwner.js
+++ src/ch06/06-1-defaultOwner.js
@@ -1,6 +1,6 @@
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
-export const getDefaultOwner = () => defaultOwnerData;
+export const getDefaultOwner = () => Object.assign({}, defaultOwnerData);
 export const setDefaultOwner = (arg) => {
   defaultOwnerData = arg;
 };
