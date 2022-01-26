7f6f4a32f91e3002440f7d6b1660a7ca04921521
mercuriver <mercuriver@gmail.com>
2022-01-26 22:09:20 +0900

    refactor(ch06): 6.6 변수 캡슐화하기, 에러 발생 상황

--- src/ch06/06-1.js
+++ src/ch06/06-1.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { getDefaultOwner, setDefaultOwner } from "./06-1-defaultOwner.js";
 
 const spaceship = {
@@ -10,3 +11,13 @@ const swordfish = {
 };
 
 console.log(spaceship, swordfish);
+
+const owner1 = getDefaultOwner();
+
+const owner2 = getDefaultOwner();
+owner2.lastName = "스피겔";
+
+assert.equal("Spiegel", owner1.lastName, "기존 값");
+assert.equal("스피겔", owner2.lastName, "변경 값");
+
+console.log({ owner1, owner2 });
