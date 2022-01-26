86d6182c7d5e0140dc9dde604b5c23aa7dddace2
mercuriver <mercuriver@gmail.com>
2022-01-26 23:43:03 +0900

    refactor(ch06): 6.11 단계 쪼개기, countOrders 함수 추출

--- src/ch06/11-2.js
+++ src/ch06/11-2.js
@@ -9,19 +9,23 @@ class Order {
   }
 }
 
+const countOrders = (args, filename) => {
+  const input = readJSON(filename);
+  const orders = input.map((item) => new Order(item));
+
+  if (args.includes("-r")) {
+    const readyOrders = orders.filter((o) => o.product.status === "ready");
+    return { flag: "ready", length: readyOrders.length };
+  } else {
+    return { flag: "not ready", length: orders.length };
+  }
+};
+
 const run = (args) => {
   try {
     if (args.length === 0) throw new Error("파일명을 입력하세요");
     const filename = args[args.length - 1];
-    const input = readJSON(filename);
-    const orders = input.map((item) => new Order(item));
-
-    if (args.includes("-r")) {
-      const readyOrders = orders.filter((o) => o.product.status === "ready");
-      console.log("ready", readyOrders.length);
-    } else {
-      console.log("not ready", orders.length);
-    }
+    return countOrders(args, filename);
   } catch (err) {
     console.error(err);
   }
@@ -29,7 +33,7 @@ const run = (args) => {
 
 const main = (args) => {
   try {
-    return run(args);
+    console.log(run(args));
   } catch (err) {
     console.error(err);
   }
