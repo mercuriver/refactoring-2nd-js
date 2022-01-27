bb9144214a9bb52cebd123ce9dc108fd28565b7c
mercuriver <mercuriver@gmail.com>
2022-01-27 23:18:41 +0900

    refactor(ch07): 7.1 레코드 캡슐화하기, 임시 게터 생성

--- src/ch07/01-1.js
+++ src/ch07/01-1.js
@@ -3,4 +3,11 @@ const organization = { name: "애크미 구스베리", country: "GB" };
 
 result += `<h1>${organization.name}</h1>`;
 
+const getRawDataOfOrganization = () => {
+  return organization;
+};
+
+getRawDataOfOrganization().name = "라일락 & 구스베리";
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+
 console.log(result);
