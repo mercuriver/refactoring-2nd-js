39100eb4afd3cc3d41696f0968da887518056acc
mercuriver <mercuriver@gmail.com>
2022-01-28 22:23:40 +0900

    refactor(ch07): 7.9 알고리즘 교체하기

--- src/ch07/09.js
+++ src/ch07/09.js
@@ -1,9 +1,5 @@
 const findPerson = (people) => {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === "Don") return "Don";
-    if (people[i] === "John") return "John";
-    if (people[i] === "Kent") return "Kent";
-  }
-  return "";
+  const candidates = ["Don", "John", "Kent"];
+  return people.find((p) => candidates.includes(p)) || "";
 };
 console.log(findPerson(["Doon", "Kay", "Johnn", "Peter", "Kent", "Clark"]));
