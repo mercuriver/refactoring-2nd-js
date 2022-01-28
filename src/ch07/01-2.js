694129a51d33235023dbeac706b77405472b91af
mercuriver <mercuriver@gmail.com>
2022-01-28 16:48:56 +0900

    refactor(ch07): 7.1 레코드 캡슐화하기, 변수 캡슐화(6.6)

--- src/ch07/01-2.js
+++ src/ch07/01-2.js
@@ -1,14 +1,18 @@
 import { readJSON } from "../util/fileController.js";
 
-const customerData = readJSON("src/ch07/01-2.json");
+let customerData = readJSON("src/ch07/01-2.json");
+
+const getRawDataOfCustomers = () => customerData;
+const setRawDataOfCustomers = (arg) => (customerData = arg);
 
 export const writeData = (customerId, year, month, amount) => {
-  customerData[customerId].usages[year][month] = amount;
+  getRawDataOfCustomers()[customerId].usages[year][month] = amount;
 };
 
 export const compareUsage = (customerId, laterYear, month) => {
-  const later = customerData[customerId].usages[laterYear][month];
-  const earlier = customerData[customerId].usages[laterYear - 1][month];
+  const later = getRawDataOfCustomers()[customerId].usages[laterYear][month];
+  const earlier =
+    getRawDataOfCustomers()[customerId].usages[laterYear - 1][month];
   return { laterAmount: later, change: later - earlier };
 };
 export const getCustomer = () => customerData;
