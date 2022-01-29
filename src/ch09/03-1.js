fc922c20a7bad70c07c6daa623114bb146c11908
mercuriver <mercuriver@gmail.com>
2022-01-29 19:35:11 +0900

    refactor(ch09): 9.3 파생 변수를 질의 함수로 바꾸기, 데이터 어서션 (ch.10.6)

--- src/ch09/03-1.js
+++ src/ch09/03-1.js
@@ -1,10 +1,16 @@
+import assert from "assert";
+
 class ProductionPlan {
   constructor() {
     this._production = 0;
     this._adjustments = [];
   }
   get production() {
+    assert(this._production === this.calculatedProduction);
     return this._production;
+  }
+  get calculatedProduction() {
+    return this._adjustments.reduce((acc, p) => acc + p.amount, 0);
   }
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
