58295db72fed4e68bc6af7172d0f0ca4da7ccd0b
mercuriver <mercuriver@gmail.com>
2022-01-29 20:09:55 +0900

    refactor(ch09): 9.4 참조를 값으로 바꾸기, 세터 제거

--- src/ch09/04.js
+++ src/ch09/04.js
@@ -1,15 +1,13 @@
 class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
   get areaCode() {
     return this._areaCode;
   }
-  set areaCode(arg) {
-    this._areaCode = arg;
-  }
   get number() {
     return this._number;
-  }
-  set number(arg) {
-    this._number = arg;
   }
 }
 
