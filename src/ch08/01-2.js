f96a067bfaf5674613f6c17a2f13557d75a81505
mercuriver <mercuriver@gmail.com>
2022-01-29 11:59:00 +0900

    refactor(ch08): 8.1 함수 옮기기, 의미에 적합한 클래스 반영

--- src/ch08/01-2.js
+++ src/ch08/01-2.js
@@ -1,3 +1,18 @@
+class AccountType {
+  constructor(isPremium) {
+    this._isPremium = isPremium;
+  }
+  // 초과 인출 이자 계산
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) return baseCharge;
+      return baseCharge + (daysOverdrawn - 7) * 0.85;
+    }
+    return daysOverdrawn * 1.75;
+  }
+}
+
 class Account {
   daysOverdrawn;
   type = { isPremium: false };
@@ -11,15 +26,8 @@ class Account {
     if (this.daysOverdrawn > 0) result += this.overdraftCharge;
     return result;
   }
-  // 초과 인출 이자 계산
-  get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this.daysOverdrawn <= 7) return baseCharge;
-      return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-    }
-    return this.daysOverdrawn * 1.75;
-  }
+
+  get overdraftCharge() {}
 }
 
 const aa = new Account(10, { isPremium: true });
