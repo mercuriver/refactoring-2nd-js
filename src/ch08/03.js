d1fe9f3d916db3edbfeebac6a7da2f230cf339a9
mercuriver <mercuriver@gmail.com>
2022-01-29 13:09:41 +0900

    refactor(ch08): 8.3 문장을 함수로 옮기기, 중복 코드 제거된 새 함수 적용

--- src/ch08/03.js
+++ src/ch08/03.js
@@ -1,8 +1,10 @@
 const renderPhoto = (aPhoto) => {
   return `<img src="${aPhoto.url}" />`;
 };
-const emitPhotoData = (aPhoto) => {
+
+const zz_emitPhotoData = (aPhoto) => {
   const result = [];
+  result.push(`<p>제목: ${aPhoto.title}</p>`);
   result.push(`<p>위치: ${aPhoto.location}</p>`);
   result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
   return result.join("\n");
@@ -12,16 +14,7 @@ const renderPerson = (person) => {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`);
-  result.push(emitPhotoData(person.photo));
-  return result.join("\n");
-};
-
-const zz_emitPhotoData = (aPhoto) => {
-  const result = [];
-  result.push(`<p>제목: ${aPhoto.title}</p>`);
-  result.push(`<p>위치: ${aPhoto.location}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
+  result.push(zz_emitPhotoData(person.photo));
   return result.join("\n");
 };
 
