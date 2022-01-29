91340dc29568b1fd9f39bb17a9e406535fea0e54
mercuriver <mercuriver@gmail.com>
2022-01-29 11:33:53 +0900

    refactor(ch08): 8.1 함수 옮기기, 중첩 함수 분해

--- src/ch08/01-1.js
+++ src/ch08/01-1.js
@@ -1,17 +1,18 @@
+const distance = (p1, p2) => Math.abs(p1 - p2);
+
+const top_calculateDistance = (points) => {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+};
+
 const trackSummary = (points) => {
   const calculateTime = () => 10000;
-  const distance = (p1, p2) => Math.abs(p1 - p2);
-
-  const calculateDistance = () => {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  };
 
   const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
+  const totalDistance = top_calculateDistance(points);
   const pace = totalTime / 60 / totalDistance;
 
   return {
