bfbb615a2f8b9d72081ee48968eba228ea60ffe3
mercuriver <mercuriver@gmail.com>
2022-01-29 12:53:45 +0900

    refactor(ch08): 8.2 필드 옮기기, 공유 값 체크

--- src/ch08/02-2.js
+++ src/ch08/02-2.js
@@ -1,7 +1,13 @@
+import assert from "assert";
+
 class Account {
-  constructor(number, type) {
+  constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    assert(
+      interestRate === this._type._interestRate,
+      "Interest rate must be the same"
+    );
   }
   get interestRate() {
     return this._type.interestRate;
@@ -18,5 +24,5 @@ class AccountType {
 }
 
 const minus = new AccountType("마이너스", 0.39);
-const acc = new Account(100000, minus);
+const acc = new Account(100000, minus, 0.39);
 console.log(acc.interestRate);
