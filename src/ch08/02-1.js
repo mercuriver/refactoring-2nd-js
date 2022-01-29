8e8397c55082adc01b7b9d292b2cebf7fd06020e
mercuriver <mercuriver@gmail.com>
2022-01-29 12:14:40 +0900

    feat(ch08): Add 02-1 base code

---
+++ src/ch08/02-1.js
@@ -0,0 +1,29 @@
+class Customer {
+  constructor(name, discountRate) {
+    this._name = name;
+    this._discountRate = discountRate;
+    this._contract = new CustomerContract(new Date());
+  }
+  get discountRate() {
+    return this._discountRate;
+  }
+  becomePreferred() {
+    this._discountRate += 0.03;
+    // 다른 멋진 일들
+  }
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this._discountRate));
+  }
+}
+
+class CustomerContract {
+  constructor(startDate) {
+    this._startDate = startDate;
+  }
+}
+
+const customer1 = new Customer("AA", 0.1);
+
+console.log(customer1.discountRate);
+customer1.becomePreferred();
+console.log(customer1.discountRate);
