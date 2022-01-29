07a26d4e561263ed4d99659102e9806ef733d23f
mercuriver <mercuriver@gmail.com>
2022-01-29 11:41:33 +0900

    refactor(ch08): 8.1 함수 옮기기, 변수 인라인하기 (ch6.4)

--- src/ch08/01-1.js
+++ src/ch08/01-1.js
@@ -1,6 +1,5 @@
-const calculateDistance = (points) => {
-  const distance = (p1, p2) => Math.abs(p1 - p2); // 거리 도출 공식, 서적에 명시된 하버사인 공식(haversine formula)과 다름
-
+const distance = (p1, p2) => Math.abs(p1 - p2); // 거리 도출 공식, 서적에 명시된 하버사인 공식(haversine formula)과 다름
+const totalDistance = (points) => {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
@@ -11,14 +10,10 @@ const calculateDistance = (points) => {
 const trackSummary = (points) => {
   const calculateTime = () => 10000;
 
-  const totalTime = calculateTime();
-  const totalDistance = calculateDistance(points);
-  const pace = totalTime / 60 / totalDistance;
-
   return {
-    time: totalTime,
-    distance: totalDistance,
-    pace,
+    time: calculateTime(),
+    distance: totalDistance(points),
+    pace: calculateTime() / 60 / totalDistance(points),
   };
 };
 
