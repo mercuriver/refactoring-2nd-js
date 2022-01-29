8308e026dceb1acf4b8bbda6ae5f571a4fd1e077
mercuriver <mercuriver@gmail.com>
2022-01-29 14:43:51 +0900

    refactor(ch08): 8.6 문장 슬라이드하기, 조건문 밖으로 빼기

--- src/ch08/06-2.js
+++ src/ch08/06-2.js
@@ -6,10 +6,11 @@ const func = () => {
   let result;
   if (availableResources.length === 0) {
     result = createResource();
-    allocatedResources.push(result);
   } else {
     result = availableResources.pop();
-    allocatedResources.push(result);
   }
+
+  allocatedResources.push(result);
+
   return result;
 };
