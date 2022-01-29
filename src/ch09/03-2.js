0a736d6276099d7e61d297f2ba8f5efbe6bc7b1b
mercuriver <mercuriver@gmail.com>
2022-01-29 19:43:01 +0900

    refactor(ch09): 9.3 파생 변수를 질의 함수로 바꾸기, 데이터 어서션 (ch.10.6)

--- src/ch09/03-2.js
+++ src/ch09/03-2.js
@@ -1,11 +1,22 @@
+import assert from "assert";
 class ProductionPlan {
   constructor(production) {
     this._adjustments = [];
     this._production = production;
+    this._initialProduction = production;
   }
   get production() {
+    assert(this._production === this.calculatedProduction);
     return this._production;
   }
+
+  get calculatedProduction() {
+    return this._adjustments.reduce(
+      (acc, p) => acc + p.amount,
+      this._initialProduction
+    );
+  }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._production += anAdjustment.amount;
@@ -16,4 +27,8 @@ const products = new ProductionPlan(0);
 products.applyAdjustment({ name: "사과", amount: 10 });
 products.applyAdjustment({ name: "바나나", amount: 20 });
 
-console.log(products.production);
+const products2 = new ProductionPlan(10);
+products2.applyAdjustment({ name: "사과", amount: 10 });
+products2.applyAdjustment({ name: "바나나", amount: 20 });
+
+console.log(products.production, products2.production);
