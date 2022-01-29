f3237f1d212946bcd478ea8bab657649aed8116f
mercuriver <mercuriver@gmail.com>
2022-01-29 12:50:43 +0900

    refactor(ch08): 8.2 필드 옮기기, 공유 객체 변경, 역할 분리

--- src/ch08/02-2.js
+++ src/ch08/02-2.js
@@ -1,18 +1,22 @@
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
+  }
+  get interestRate() {
+    return this._type.interestRate;
+  }
+}
+class AccountType {
+  constructor(nameString, interestRate) {
+    this._name = nameString;
     this._interestRate = interestRate;
   }
   get interestRate() {
     return this._interestRate;
   }
 }
-class AccountType {
-  constructor(nameString) {
-    this._name = nameString;
-  }
-}
-const minus = new AccountType("마이너스");
-const acc = new Account(100000, minus, 0.39);
+
+const minus = new AccountType("마이너스", 0.39);
+const acc = new Account(100000, minus);
 console.log(acc.interestRate);
