f5e9d74688bcef5a3fe5a532fb99ac19d6fc0514
mercuriver <mercuriver@gmail.com>
2022-01-29 18:17:02 +0900

    refactor(ch08): 8.8 반복문 파이프라인화, 함수 인라인하기

--- src/ch08/08.js
+++ src/ch08/08.js
@@ -5,14 +5,13 @@ Banalore, India, +91 80 4064 9570
 Porto Alegre, Brazil, +55 51 3079 3550
 Chennai, India, +91 44 660 44766`;
 
-const acquireData = (input) => {
-  const lines = input.split("\n");
-  return lines
+const acquireData = (input) =>
+  input
+    .split("\n")
     .slice(1)
     .filter((line) => line.trim() !== "")
     .map((line) => line.split(","))
     .filter((record) => record[1].trim() === "India")
     .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
-};
 
 console.log(acquireData(csvData));
