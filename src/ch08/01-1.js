628a17e224f814b2f61f984b84ad3f358f6c83df
mercuriver <mercuriver@gmail.com>
2022-01-29 11:38:35 +0900

    refactor(ch08): 8.1 함수 옮기기, 관련 함수 중첩 함수로 구성

--- src/ch08/01-1.js
+++ src/ch08/01-1.js
@@ -1,6 +1,6 @@
-const distance = (p1, p2) => Math.abs(p1 - p2);
+const calculateDistance = (points) => {
+  const distance = (p1, p2) => Math.abs(p1 - p2); // 거리 도출 공식, 서적에 명시된 하버사인 공식(haversine formula)과 다름
 
-const top_calculateDistance = (points) => {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
@@ -12,7 +12,7 @@ const trackSummary = (points) => {
   const calculateTime = () => 10000;
 
   const totalTime = calculateTime();
-  const totalDistance = top_calculateDistance(points);
+  const totalDistance = calculateDistance(points);
   const pace = totalTime / 60 / totalDistance;
 
   return {
