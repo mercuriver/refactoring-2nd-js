d318316f345ff72ffc585034c1397771aebad9ce
mercuriver <mercuriver@gmail.com>
2022-01-30 15:39:46 +0900

    refactor(ch10): 10.3 중첩 조건문을 보호 구문으로 바꾸기

--- src/ch10/03-1.js
+++ src/ch10/03-1.js
@@ -1,16 +1,7 @@
 const payAmount = (employee) => {
-  let result;
-  if (employee.isSeperated) result = { amount: 0, reasonCode: "SEP" };
-  // 퇴사한 직원
-  else {
-    if (employee.isRetired) result = { amount: 0, reasonCode: "RET" };
-    // 은퇴한 직원
-    else {
-      // 급여 계산 로직
-      result = { amount: 100, reasonCode: "WRK" }; // 재직
-    }
-  }
-  return result;
+  if (employee.isSeperated) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  return { amount: 100, reasonCode: "WRK" };
 };
 
 console.log(payAmount({}));
