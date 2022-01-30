cd0d0b03add004af96ba7ba220395015a0b77b94
mercuriver <mercuriver@gmail.com>
2022-01-30 15:20:31 +0900

    refactor(ch10): 10.1 조건식 통합하기

--- src/ch10/02-1.js
+++ src/ch10/02-1.js
@@ -1,6 +1,18 @@
 const disabilityAmount = (anEmployee) => {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
-  // 장애 수당 계산
+  if (
+    anEmployee.seniority < 2 ||
+    anEmployee.monthsDisabled > 12 ||
+    anEmployee.isPartTime
+  ) {
+    return 0;
+  }
+  return 1;
 };
+
+console.log(
+  disabilityAmount({
+    seniority: 2,
+    monthsDisabled: 13,
+    isPartTime: false,
+  })
+);
