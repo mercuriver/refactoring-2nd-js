49bcd269bce9c9bcfbf215ea09a9f376e5f0347c
mercuriver <mercuriver@gmail.com>
2022-01-30 15:22:03 +0900

    refactor(ch10): 10.2 조건식 통합하기, 함수 추출하기 (ch6.1)

--- src/ch10/02-1.js
+++ src/ch10/02-1.js
@@ -1,18 +1,26 @@
-const disabilityAmount = (anEmployee) => {
-  if (
+function isNotEligibleForDisability(anEmployee) {
+  return (
     anEmployee.seniority < 2 ||
     anEmployee.monthsDisabled > 12 ||
     anEmployee.isPartTime
-  ) {
-    return 0;
-  }
-  return 1;
-};
+  );
+}
+
+const disabilityAmount = (anEmployee) =>
+  isNotEligibleForDisability(anEmployee) ? 0 : 1;
 
 console.log(
   disabilityAmount({
     seniority: 2,
     monthsDisabled: 13,
+    isPartTime: false,
+  })
+);
+
+console.log(
+  disabilityAmount({
+    seniority: 2,
+    monthsDisabled: 10,
     isPartTime: false,
   })
 );
