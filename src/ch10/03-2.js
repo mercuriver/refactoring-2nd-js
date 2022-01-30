6298a0b00470d14ceb7f83645e9285a6e1e91607
mercuriver <mercuriver@gmail.com>
2022-01-30 16:00:55 +0900

    refactor(ch10): 10.3 중첩 조건문을 보호 구문으로 바꾸기, 조건 반대로 만들기

--- src/ch10/03-2.js
+++ src/ch10/03-2.js
@@ -1,21 +1,29 @@
 const adjustCapital = (anInstrument) => {
   let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result =
-        (anInstrument.income / anInstrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
-  }
+  if (anInstrument.capital <= 0) return result;
+  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0)
+    return result;
+
+  result =
+    (anInstrument.income / anInstrument.duration) *
+    anInstrument.adjustmentFactor;
+
   return result;
 };
 
-console.log(adjustCapital({ income: 10, adjustmentFactor: 2 }));
-console.log(adjustCapital({ income: 10, adjustmentFactor: 2, capital: 1 }));
+const instrument = {
+  income: 10,
+  adjustmentFactor: 2,
+  capital: 0,
+  interestRate: 0,
+  duration: 0,
+};
+
+console.log(adjustCapital(instrument));
+console.log(adjustCapital({ ...instrument, capital: 1 }));
 console.log(
   adjustCapital({
-    income: 10,
-    adjustmentFactor: 2,
+    ...instrument,
     capital: 1,
     interestRate: 1,
     duration: 1,
