d2589e5a6585eca29ca11aeba2d3afc4070ec65c
mercuriver <mercuriver@gmail.com>
2022-01-31 02:33:08 +0900

    feat(baseCode): Add ch11 base code

---
+++ baseCode/ch11/01.js
@@ -0,0 +1,16 @@
+const setOffAlarms = () => {
+  console.warn("악당을 찾았소");
+};
+const alertForMiscreant = (people) => {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+      return "조커";
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+  return "";
+};
