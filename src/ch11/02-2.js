1cdf7935326076cde3c43539a6e8c31326a37545
mercuriver <mercuriver@gmail.com>
2022-01-31 02:19:04 +0900

    refactor(ch11): 11.2 함수 매개변수화하기, 알고리즘 교체하기 (ch7.9)

--- src/ch11/02-2.js
+++ src/ch11/02-2.js
@@ -12,8 +12,11 @@ const topBand = (usage) => (usage > 200 ? usage - 200 : 0);
 const withinBand = (usage, bobttom, top) =>
   usage > bobttom ? Math.min(usage, top) - bobttom : 0;
 
+const baseCharge = (usage) => {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    bottomBand(usage) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    topBand(usage) * 0.07;
   return usd(amount);
 };
