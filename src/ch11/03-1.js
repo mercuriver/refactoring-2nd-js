bf15e1bb414268c354157c285ed0936f5149f209
mercuriver <mercuriver@gmail.com>
2022-01-31 02:29:23 +0900

    refactor(ch11): 11.3 플래그 인수 제거하기, 조건문 분해하기 (ch10.1)

--- src/ch11/03-1.js
+++ src/ch11/03-1.js
@@ -11,21 +11,25 @@ class Order {
     this.placedOn = new Place();
   }
 }
-const deliveryDate = (anOrder, isRush) => {
-  if (isRush) {
-    let deliveryTime;
-    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-  } else {
-    let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
+
+const rushDeliveryDate = (anOrder) => {
+  let deliveryTime;
+  if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else deliveryTime = 3;
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
 };
+
+const regularDeliveryDate = (anOrder) => {
+  let deliveryTime;
+  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+  else deliveryTime = 4;
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+};
+
+const deliveryDate = (anOrder, isRush) =>
+  isRush ? rushDeliveryDate(anOrder) : regularDeliveryDate(anOrder);
 
 console.log(deliveryDate(new Order("MA"), true));
 console.log(deliveryDate(new Order("NH"), true));
