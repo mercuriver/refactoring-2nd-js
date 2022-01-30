4a6db8416c35e9bbe1a65acad10fe4fe78eacc42
mercuriver <mercuriver@gmail.com>
2022-01-31 02:50:50 +0900

    refactor(ch11): 11.4 객체 통째로 넘기기, 함수 옮기기 (ch8.1)

--- src/ch11/04-2.js
+++ src/ch11/04-2.js
@@ -19,20 +19,19 @@ class HeatingPlan {
       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
     );
   }
+  xxNewWithinRange(tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    return this.withinRange(low, high);
+  }
 }
-
-const xxNewWithinRange = (aPlan, tempRange) => {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  return aPlan.withinRange(low, high);
-};
 
 const client = () => {
   const room = new Room(22, 24);
   const plan = new HeatingPlan(21, 25);
   const tempRange = room.daysTempRange;
 
-  const isWithinRange = xxNewWithinRange(plan, tempRange);
+  const isWithinRange = plan.xxNewWithinRange(tempRange);
 
   if (!isWithinRange) {
     console.log("방 온도가 지정 범위를 벗어났습니다.");
