cc711cd6fc98b6267c04cec5c12209cb39136ade
mercuriver <mercuriver@gmail.com>
2022-01-31 02:07:31 +0900

    refactor(ch11): 11.1 질의 함수와 변경 함수 분리하기

--- src/ch11/01.js
+++ src/ch11/01.js
@@ -1,21 +1,30 @@
 const setOffAlarms = () => {
   console.warn("악당을 찾았소");
 };
-const alertForMiscreant = (people) => {
+const findMiscreant = (people) => {
   for (const p of people) {
     if (p === "조커") {
-      setOffAlarms();
       return "조커";
     }
     if (p === "사루만") {
-      setOffAlarms();
       return "사루만";
     }
   }
   return "";
 };
+const alertForMiscreant = (people) => {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+    }
+  }
+  return;
+};
 
-alertForMiscreant([
+const people = [
   "슈퍼맨",
   "배트맨",
   "아이언맨",
@@ -23,4 +32,6 @@ alertForMiscreant([
   "블랙위도우",
   "조커",
   "스파이더맨",
-]);
+];
+const found = findMiscreant(people);
+alertForMiscreant(people);
