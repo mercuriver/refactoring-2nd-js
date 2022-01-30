f4a5e5268b9cee1176eafd6ce3ef2aa72e6bba9b
mercuriver <mercuriver@gmail.com>
2022-01-30 15:24:11 +0900

    refactor(ch10): 10.2 조건식 통합하기

--- src/ch10/02-2.js
+++ src/ch10/02-2.js
@@ -1,6 +1,23 @@
 const func = (anEmployee) => {
-  if (anEmployee.onVacation) {
-    if (anEmployee.seniority > 10) return 1;
-  }
+  if (anEmployee.onVacation && anEmployee.seniority > 10) return 1;
   return 0.5;
 };
+
+console.log(
+  func({
+    onVacation: false,
+    seniority: 2,
+  }),
+  func({
+    onVacation: false,
+    seniority: 20,
+  }),
+  func({
+    onVacation: true,
+    seniority: 2,
+  }),
+  func({
+    onVacation: true,
+    seniority: 20,
+  })
+);
