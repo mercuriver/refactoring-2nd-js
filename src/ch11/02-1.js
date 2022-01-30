d1179fdd1a505049e6490f425276e5441ec58ec1
mercuriver <mercuriver@gmail.com>
2022-01-31 02:14:39 +0900

    refactor(ch11): 11.2 함수 매개변수화하기, 커리 함수를 통한 코드 개선

--- src/ch11/02-1.js
+++ src/ch11/02-1.js
@@ -1,10 +1,6 @@
-const tenPercentRaise = (person) => {
-  person.salary = person.salary.multiply(1.1);
-};
-const fivePercentRaise = (person) => {
-  person.salary = person.salary.multiply(1.05);
-};
-
 const raise = (factor) => (person) => {
   person.salary = person.salary.multiply(1 + factor);
 };
+
+const tenPercentRaise = raise(1.1);
+const fivePercentRaise = raise(1.05);
