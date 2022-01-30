5aad097d57548225215f9368ec7b23e65ac5d0ed
mercuriver <mercuriver@gmail.com>
2022-01-31 02:41:19 +0900

    refactor(ch11): 11.4 객체 통째로 넘기기, 새 함수 적용

--- src/ch11/04-1.js
+++ src/ch11/04-1.js
@@ -15,12 +15,7 @@ class HeatingPlan {
   constructor(low, high) {
     this._temperatureRange = new TemperatureRange(low, high);
   }
-  withinRange(bottom, top) {
-    return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
-    );
-  }
-  xxNewWithinRange(aNumberRange) {
+  withinRange(aNumberRange) {
     return (
       aNumberRange.low >= this._temperatureRange.low &&
       aNumberRange.high <= this._temperatureRange.high
@@ -30,7 +25,7 @@ class HeatingPlan {
 
 const client = () => {
   const plan = new HeatingPlan(21, 25);
-  if (!plan.xxNewWithinRange(room.daysTempRange)) {
+  if (!plan.withinRange(room.daysTempRange)) {
     console.log("방 온도가 지정 범위를 벗어났습니다.");
   } else {
     console.log("적정 온도입니다.");
