9233fad533f5b9dd9e4d003c7bd485862f4fcfa6
mercuriver <mercuriver@gmail.com>
2022-01-30 16:26:42 +0900

    refactor(ch10): 10.4 조건부 로직을 다형성으로 바꾸기, 서브클래스 getter 오버라이드

--- src/ch10/04-1.js
+++ src/ch10/04-1.js
@@ -7,35 +7,40 @@ class Bird {
   }
 
   get feather() {
-    switch (this.type) {
-      case "유럽 제비":
-        return "보통이다";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무":
-        return this.voltage > 100 ? "그을렸다" : "예쁘다";
-      default:
-        return "알 수 없다";
-    }
+    return "알 수 없다";
   }
 
   get velocity() {
-    switch (this.type) {
-      case "유럽 제비":
-        return 35;
-      case "아프리카 제비":
-        return 40 - 2 * this.numberOfCoconuts;
-      case "노르웨이 파랑 앵무":
-        return this.isNailed ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
 
-class EuropeanSwallow extends Bird {}
-class AfricanSwallow extends Bird {}
-class NorwegianBlueParrot extends Bird {}
+class EuropeanSwallow extends Bird {
+  get feather() {
+    return "보통이다";
+  }
+  get velocity() {
+    return 35;
+  }
+}
+class AfricanSwallow extends Bird {
+  get feather() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
+
+  get velocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  get feather() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+
+  get velocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
 
 const createBiard = (bird) => {
   switch (bird.type) {
