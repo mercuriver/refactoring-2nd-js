ac81dcb418deac6f4ab8ab6844f7a1a9e9c2add7
mercuriver <mercuriver@gmail.com>
2022-01-31 16:05:28 +0900

    refactor(ch11): 11.9 함수를 명령으로 바꾸기, 지역변수 필드로 변경

--- src/ch11/09.js
+++ src/ch11/09.js
@@ -6,22 +6,28 @@ class Score {
   }
 
   execute() {
-    let result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
+    this._result = 0;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
+
     if (this._medicalExam.isSmoker) {
-      healthLevel += 10;
-      highMedicalRiskFlag = true;
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
     }
     let certificationGrade = "regular";
     if (
       this._scoringGuide.stateWithLowCertification(this._candidate.originState)
     ) {
       certificationGrade = "low";
-      result -= 5;
+      this._result -= 5;
     }
-    result -= Math.max(healthLevel - 5, 0);
-    return { result, certificationGrade, highMedicalRiskFlag };
+    this._result -= Math.max(this._healthLevel - 5, 0);
+
+    return {
+      result: this._result,
+      certificationGrade,
+      highMedicalRiskFlag: this._highMedicalRiskFlag,
+    };
   }
 }
 
