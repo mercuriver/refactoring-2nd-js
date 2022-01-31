ee24813e76c7cb2c2495a384fdb4a2e5623b2590
mercuriver <mercuriver@gmail.com>
2022-01-31 15:58:41 +0900

    refactor(ch11): 11.9 함수를 명령으로 바꾸기

--- src/ch11/09.js
+++ src/ch11/09.js
@@ -1,18 +1,24 @@
+class Score {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    result -= Math.max(healthLevel - 5, 0);
+    return { result, certificationGrade, highMedicalRiskFlag };
+  }
+}
+
 const score = (candidate, medicalExam, scoringGuide) => {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
-  }
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
-  }
-  result -= Math.max(healthLevel - 5, 0);
-  return { result, certificationGrade, highMedicalRiskFlag };
+  return new Score().execute(candidate, medicalExam, scoringGuide);
 };
 
 const scoringGuide = {
