79f1c8c3d8f9803d2118b56a56393dbc2987a814
mercuriver <mercuriver@gmail.com>
2022-01-31 16:20:50 +0900

    refactor(ch11): 11.11 수정된 값 반환하기

--- src/ch11/11.js
+++ src/ch11/11.js
@@ -3,15 +3,20 @@ const calculateAscent = () => {
     const verticalChange = points[i].elevation - points[i - 1].elevation;
     totalAscent += verticalChange > 0 ? verticalChange : 0;
   }
+  return totalAscent;
 };
 const calculateTime = () => {};
 const calculateDistance = () => {};
 
-let points = [];
+let points = [{ elevation: 1 }, { elevation: 4 }, { elevation: 9 }];
 let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
+
+totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
+
 const pace = totalTime / 60 / totalDistance;
+
+console.log(totalAscent, pace);
