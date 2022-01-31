ec551d6634362baa0052114e73b67430b75de4e3
mercuriver <mercuriver@gmail.com>
2022-01-31 16:41:23 +0900

    refactor(ch11): 11.12 오류 코드를 예외로 바꾸기, 에러 핸들 클레스 작성

--- src/ch11/12.js
+++ src/ch11/12.js
@@ -11,15 +11,25 @@ const countryData = {
 };
 const errorList = [];
 
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super(`주문 처리 오류 ${errorCode}`);
+    this.code = errorCode;
+  }
+  get name() {
+    return "OrderProcessingError";
+  }
+}
+
 const localShippingRules = (country) => {
   const data = countryData.shippingRules[country];
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
 };
 const calculateShippingCosts = (order) => {
   // 관련 없는 코드
   const shippingRules = localShippingRules(order.country);
-  if (shippingRules < 0) return shippingRules;
+  if (shippingRules < 0) throw new Error("오류 코드가 다 사라지지 않았습니다.");
   // 관련 없는 코드
 };
 const execute = (order) => {
@@ -27,9 +37,12 @@ const execute = (order) => {
   try {
     state = calculateShippingCosts(order);
   } catch (err) {
-    throw err;
+    if (err instanceof OrderProcessingError) {
+      if (state < 0) errorList.push({ order, errorCode: state });
+    } else {
+      throw err;
+    }
   }
-  if (state < 0) errorList.push({ order, errorCode: state });
 };
 
 execute({ country: "US" });
