61b44d706457a3639edc4684296dcf30a91c338c
mercuriver <mercuriver@gmail.com>
2022-02-01 11:28:00 +0900

    fix(ch12): 예제 코드 오류 정정

--- src/ch12/02.js
+++ src/ch12/02.js
@@ -1,7 +1,10 @@
 class Employee {}
 class SalesPerson extends Employee {
-  name = "";
+  #name = "SalesPerson name";
 }
 class Engineer extends Employee {
-  name = "";
+  #name = "Engineer name";
 }
+
+console.log(new SalesPerson().#name);
+console.log(new Engineer().#name);
