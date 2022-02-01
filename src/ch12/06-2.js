82c4c4dda5024e100c9b900f09b7e33a905ca607
mercuriver <mercuriver@gmail.com>
2022-02-01 13:50:21 +0900

    refactor(ch12): 12.6 타입 코드를 서브클래스로 바꾸기

--- src/ch12/06-2.js
+++ src/ch12/06-2.js
@@ -1,9 +1,18 @@
-class EmployeeType {
-  constructor(str) {
-    this._value = str;
-  }
+class EmployeeType {}
+
+class Engineer extends EmployeeType {
   toString() {
-    return this._value;
+    return "engineer";
+  }
+}
+class Manager extends EmployeeType {
+  toString() {
+    return "manager";
+  }
+}
+class Salesperson extends EmployeeType {
+  toString() {
+    return "salesperson";
   }
 }
 
@@ -13,6 +22,19 @@ class Employee {
     this._name = name;
     this._type = type;
   }
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer":
+        return new Engineer();
+      case "manager":
+        return new Manager();
+      case "salesperson":
+        return new Salesperson();
+      default:
+        throw new Error(`${aString}라는 직원 유형은 없습니다.`);
+    }
+  }
+
   validateType(arg) {
     if (!["engineer", "manager", "salesperson"].includes(arg))
       throw new Error(`${arg}라는 직원 유형은 없습니다.`);
@@ -20,21 +42,20 @@ class Employee {
   get typeString() {
     return this._type.toString();
   }
-
   get type() {
     return this._type;
   }
   set type(arg) {
-    this._type = arg;
+    this._type = Employee.createEmployeeType(arg);
   }
-  get capitalizedType() {
+  get capitalizedName() {
     return (
       this.typeString.charAt(0).toUpperCase() +
       this.typeString.slice(1).toLowerCase()
     );
   }
   toString() {
-    return `${this._name} is a ${this.capitalizedType}`;
+    return `${this._name} is a ${this.capitalizedName}`;
   }
 }
 
